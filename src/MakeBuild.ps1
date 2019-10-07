01a00e17cba2094c7d62c3fb17c314cb1f849495
liguori <gianluigiliguori@live.it>
2019-10-07 16:17:22 +0200

    Node package update

--- src/MakeBuild.ps1
+++ src/MakeBuild.ps1
@@ -1,4 +1,4 @@
-dotnet publish .\EngagementOrganizer.API\EngagementOrganizer.API.sln --self-contained --runtime win-x64 -o ..\..\release\Services
+dotnet publish .\EngagementOrganizer.API\EngagementOrganizer.API.sln --self-contained --runtime win-x64 -o .\release\Services
 cd .\EngagementOrganizer.SPA
 npm install
 ng build --prod --aot --outputPath=..\release\Application --configuration='production'
