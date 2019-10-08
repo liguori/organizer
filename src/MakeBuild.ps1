101041fe10b6ef849ca0c223824651138b942431
liguori <gianluigiliguori@live.it>
2019-10-08 15:18:55 +0200

    Build for electron support

--- src/MakeBuild.ps1
+++ src/MakeBuild.ps1
@@ -1,5 +1,12 @@
-dotnet publish .\EngagementOrganizer.API\EngagementOrganizer.API.sln --self-contained --runtime win-x64 -o .\release\Services
+$folderService=".\EngagementOrganizer.App\Services"
+$fodlerUI=".\EngagementOrganizer.App\UI"
+if (Test-Path $folderService) { Remove-Item -LiteralPath $folderService -Force -Recurse }
+if (Test-Path $fodlerUI) { Remove-Item -LiteralPath $fodlerUI -Force -Recurse }
+
+dotnet publish .\EngagementOrganizer.API\EngagementOrganizer.API.sln -o $folderService
 cd .\EngagementOrganizer.SPA
 npm install
-ng build --prod --aot --outputPath=..\release\Application --configuration='production'
-cd..
+ng build --prod --base-href ./ --outputPath=".$fodlerUI" --configuration='production'
+cd ..\EngagementOrganizer.APP
+.\node_modules\.bin\gulp
+cd ..
