64c8c710b74635e92f9b1a37947c0049b37f74a9
liguori <gianluigiliguori@live.it>
2020-05-29 20:01:52 +0200

    NuGet packaged updates

--- src/EngagementOrganizer.API/EngagementOrganizer.API/Models/Appointment.cs
+++ src/EngagementOrganizer.API/EngagementOrganizer.API/Models/Appointment.cs
@@ -19,6 +19,8 @@ namespace EngagementOrganizer.API.Models
 
         public int TypeID { get; set; }
 
+        public int? AvailabilityID { get; set; }
+
         public DateTime StartDate { get; set; }
 
         public DateTime EndDate { get; set; }
