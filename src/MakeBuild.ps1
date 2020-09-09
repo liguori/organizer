cba6bc3f108f0e6bd0a2b6e63d0ed09ed351fcbe
Gianluigi Liguori <gianluigiliguori@live.it>
2020-09-09 18:07:17 +0200

    Fix case sensitive script

--- src/MakeBuild.ps1
+++ src/MakeBuild.ps1
@@ -7,7 +7,7 @@ dotnet publish .\EngagementOrganizer.API\EngagementOrganizer.API.sln -o $folderS
 cd .\EngagementOrganizer.SPA
 npm install
 npm run build -- --prod --base-href ./ --outputPath=".$fodlerUI" --configuration='production'
-cd ..\EngagementOrganizer.APP
+cd ..\EngagementOrganizer.App
 npm install
 .\node_modules\.bin\gulp
 npm run dist
