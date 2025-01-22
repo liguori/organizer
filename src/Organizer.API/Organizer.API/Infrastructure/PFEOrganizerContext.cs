715e5057157bd0945a8f846042615b462f557660
Gianluigi Liguori <gianluigiliguori@live.it>
2025-01-22 15:35:37 +0100

    Add new event type

--- src/Organizer.API/Organizer.API/Infrastructure/PFEOrganizerContext.cs
+++ src/Organizer.API/Organizer.API/Infrastructure/PFEOrganizerContext.cs
@@ -23,6 +23,8 @@ namespace Organizer.API.Infrastructure
                  new AppointmentType { ID = 5, Description = "National Celebration", Billable = false, RequireCustomer = false, Color = "#efb8b8", TextColor = "#000000", ShortDescription = "CEL" },
                  new AppointmentType { ID = 6, Description = "Company Event", Billable = false, RequireCustomer = false, Color = "#3087c1", TextColor = "#ffffff", ShortDescription = "COM" },
                  new AppointmentType { ID = 7, Description = "Blocker", Billable = false, RequireCustomer = false, Color = "#97ff8c", TextColor = "#000000", ShortDescription = "B" },
+                 new AppointmentType { ID = 8, Description = "Concert", Billable = false, RequireCustomer = false, Color = "#ffa200", TextColor = "#000000", ShortDescription = "CON" },
+                 new AppointmentType { ID = 9, Description = "Travel", Billable = false, RequireCustomer = false, Color = "#4b8773", TextColor = "#FFFFFF", ShortDescription = "TRV" },
                  new AppointmentType { ID = 99, Description = "Upstream Calendar", Billable = true, RequireCustomer = false, Color = "#0000ff", TextColor = "#ffffff", ShortDescription = "Upstream" }
             );
             modelBuilder.Entity<Calendar>().HasKey(x => x.CalendarName);
